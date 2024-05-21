96b8a1ea661ed83317d7c63dacda8c4a29ac9ca4
unknown <anubhavgoyal12062001@gmail.com>
2024-05-21 10:51:21 +0000

    Final Push

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
         props.showAlert("Converted to uppercase!", "success");
@@ -21,12 +22,17 @@ export default function TextForm(props) {
     }
 
     const handleOnChange = (event)=>{
+        // console.log("On change");
         setText(event.target.value) 
     }
 
     // Credits: A
     const handleCopy = () => {
-        navigator.clipboard.writeText(text); 
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select(); 
+        navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success");
     }
 
@@ -55,8 +61,8 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
